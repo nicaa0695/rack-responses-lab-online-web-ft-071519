2cb68741135959427aaf95ada2815394223be7b4
Veronica Lopez <nicaa0695@gmail.com>
2019-08-23 20:29:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     
     if Time.now.hour.between?(0, 11)
     resp.write "Good Morning!"
-    elsif Time.now.hour.between(12, 17)
+    elsif Time.now.hour.between?(12, 17)
     resp.write "Good Afternoon!"
     end   
     resp.finish 
